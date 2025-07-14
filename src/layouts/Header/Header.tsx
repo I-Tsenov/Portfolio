27a69b6af7458bf1f8445a1aba51e856a9f3d608
I.Tsenov <tobetsenov@gmail.com>
2025-07-14 16:21:02 +0300

    theme adjustments

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import ThemeToggleBtn from '@components/ThemeToggleBtn';
-import styles from './Header.module.scss'
-
+import styles from './Header.module.scss';
 
 const navLinks = [
     { to: '/', label: 'Home' },
@@ -16,11 +15,7 @@ const Header: React.FC = () => {
 
     return (
         <header className={styles.header}>
-            <Link
-                className={styles.logoHeader}
-                key='logo_header'
-                to='/'
-            >
+            <Link className={styles.logoHeader} key="logo_header" to="/">
                 {'< IT >'}
             </Link>
             <nav>
@@ -28,13 +23,7 @@ const Header: React.FC = () => {
                     <Link
                         key={link.to}
                         to={link.to}
-                        style={{
-                            marginRight: '1rem',
-                            fontSize: '1.2rem',
-                            textDecoration: 'none',
-                            color: location.pathname === link.to ? 'rgb(6, 165, 149)' : '#fff',
-                            fontWeight: location.pathname === link.to ? 'bold' : 'normal',
-                        }}
+                        className={location.pathname === link.to ? styles.navItemActive : styles.navItem}
                     >
                         {link.label}
                     </Link>
